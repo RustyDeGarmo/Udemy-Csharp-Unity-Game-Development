775e5ffe9dc895be539d145e19b0105d5508946a
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2023-05-06 18:57:51 -0500

    Created our bricky character with a new material to make it brick-ish colored and added a new material to our ground plane to make it grassy green. Also added movement (x,y,z) variables to a script and serialized them so they are accessible from withing Unity.

--- Bricky Road/Assets/Mover.cs
+++ Bricky Road/Assets/Mover.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float xValue = 0.01f;
+    [SerializeField] float yValue = 0.00f;
+    [SerializeField] float zValue = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(xValue, yValue, zValue);
     }
 }
