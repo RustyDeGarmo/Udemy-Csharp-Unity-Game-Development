b4004e8aa6d11c3a276ad6639ee1e2249579df2a
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2023-06-10 14:05:26 -0500

    added two new levels. added a method to restart the current level if we hit an obstacle. Added a method to load the next level if we safely make it to the landing pad

--- Ship Shapes/Ship Shapes/Assets/Scripts/CollisionHandler.cs
+++ Ship Shapes/Ship Shapes/Assets/Scripts/CollisionHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -11,15 +12,28 @@ public class CollisionHandler : MonoBehaviour
             case "Friendly":
                 Debug.Log("This thing is friendly");
                 break;
-            case "Fuel":
-                Debug.Log("Refueled! Let's go!");
-                break;
             case "Finish":
-                Debug.Log("You finished, you're a winner!");
+                Debug.Log("You have made it through this challenge!");
+                LoadNextLevel();
                 break;
             default:
-                Debug.Log("Kaboom, you blew up!"); 
+                ReloadLevel();
                 break;
         }
+    }
+
+    int getCurrentLevel()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
+    void ReloadLevel()
+    {
+        //reload current level on death
+        SceneManager.LoadScene(getCurrentLevel());
+    }
+
+    void LoadNextLevel(){
+        //load the next level on win
+        SceneManager.LoadScene(getCurrentLevel() + 1);
     }
 }
